943d6a82cea8b11e98b9dfa079b1a0ea6b6b973f
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2025-05-20 23:36:22 -0700

    issues

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async(req, res) => {
-    console.log("Register route hit ✅");
-    res.status(200).json({
-        message : "mosi code working"
-    })
+   // get user details from frontend 
+   // validation - not empty
+   // check if user already exits : username & email 
+   // check for images and check for avatar
+   // upload them to cloudinary => check url from "uploadResult" & avatar
+   // create user object (nosql) - create entry in db
+   // remove password and refresh token fields from "uploadResult"
+   // check for user creation 
+   // return res
+
+  const {username, email, password, fullName} = req.body                       // data coming from in form and json then .body
 })
 
 export {registerUser}
--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+import dns from "dns";
+
+// Set custom DNS servers (Google DNS)
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const connectDB = async () => {
     try {
         const connectionIntance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
