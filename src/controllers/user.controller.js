af0c5a1538b8a1be11fe170bf8bd4ba3ceadc5cb
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2025-05-31 23:55:49 -0700

    Refactor changeCurrentPassword function to improve readability and use $unset for refreshToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -171,8 +171,11 @@ const logoutUser = asyncHandler( async ( req, res ) => {
   await User.findByIdAndUpdate(
           req.user._id,
           {
-            $set: {
-               refreshToken : undefined
+            // $set: {
+            //    refreshToken : undefined
+            // }
+            $unset: {
+               refreshToken : 1   // this removes the fields from documents
             }
           },
           {
@@ -239,22 +242,24 @@ const refreshAccessToken = asyncHandler( async (req, res) => {
   }
 })
 
-const changeCurrentPassword = asyncHandler( async(req, res)=> {
-  const {oldPassword, newPassword}= req.body
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    const {oldPassword, newPassword} = req.body
 
- const user =  await User.findById(  req.user?._id )
- const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    
 
- if (!isPasswordCorrect) {
-  throw new ApiError(400, "Invalid old password")
- }
- user.password= newPassword
- await user.save({validateBeforeSave : false})
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
 
- return res 
- .status(200)
- .json( new ApiResponse(200, {}, "Password changed successfully"))
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password")
+    }
 
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"))
 })
 
 const getCurrentUser = asyncHandler( async (req, res) =>{
